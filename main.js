4dbd1212e2a9be2979f86c7c6e13535d47990bd1
ydevil2009 <85282146+ydevil2009@users.noreply.github.com>
2022-04-08 15:28:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 song = "";
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 leftWristX = 0;
 leftWristY = 0;
-rightWristY = 0;
-rightWristX = 0;
 
-function preload()
-{
+function preload() {
     song = loadSound("music.mp3");
 }
 
-function setup()
-{
+function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
 
@@ -24,10 +25,20 @@ function setup()
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals / 500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
-function play()
-{
+function play() {
     song.play();
     song.setVolume(1);
     song.rate(1);
@@ -37,17 +48,18 @@ function modelLoaded() {
     console.log('Posenet Is Initialized');
 }
 
-function gotPoses(results)
-{
-    if(results.lenght > 0)
-    {
+function gotPoses(results) {
+    if (results.lenght > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
 
-rightWristX = results[0].pose.rightWrist.x;
-rightWristY = results[0].pose.rightWrist.y;
-console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
     }
 }
